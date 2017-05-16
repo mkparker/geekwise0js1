a0d8469b87e7249e295a6a10acc972ba43016159
Mato-Kuwapi Parker <Mato-Kuwapi.Parker@fresnounified.org>
2017-05-15 20:57:34 -0700

    day01

---
+++ day01/js/script.js
@@ -0,0 +1,71 @@
+// ES6 - ECMASript6
+
+// Good
+var lowercase;
+var camelCase;
+var use_underscores;
+var _underscores;
+var $dollarsign;
+
+// Meh
+var SuperCamelCase;
+
+// Bad
+//var no spaces;
+//var no-hypens;
+//var 1no-numbers;
+
+console.log('Sending a message to the console');
+console.info('Sending an information message.');
+console.warn('Heres a warning');
+console.error('Danger Will Robinson!');
+
+// console.log();
+
+// Single line Comment
+
+/*
+    Multi
+    Line
+    Comments
+*/
+
+var
+
+
+
+
+
+
+            freedom
+
+
+
+
+=
+
+
+
+
+
+
+
+
+"A string of" +
+" " +
+"javascript";
+
+console.log(freedom);
+
+// template string
+
+var javascriptTemplateString = `
+and
+type
+whatever
+you
+want
+:)
+`;
+
+console.log(javascriptTemplateString);
